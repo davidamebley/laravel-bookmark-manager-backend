a1ce47f78ef6e4d4818b563f7659a2f68b3e4e57
David Amebley <davameb@gmail.com>
2022-11-02 02:44:50 +0200

    Create fillables for Models

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Bookmark extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'url',
+        'folder_id'
+    ];
 }
